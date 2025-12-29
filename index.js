78f0d71e59fd6fa38ddd82534e9b1f705e23cda0
Diego Ibarra <diego12el@hotmail.com>
2025-12-29 19:21:09 -0300

    FSO 3.6 added validations for the persons post endpoint

--- index.js
+++ index.js
@@ -65,10 +65,23 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
-
-if (!body || !body.name || !body.number) {
+    
+  if (!body.name) 
+  {
     return response.status(400).json({ 
-      error: 'content missing' 
+      error: 'name missing' 
+    })
+  }
+  else if (!body.number) 
+  {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+  else if(persons.find(person => person.name.toLowerCase() === body.name.toLowerCase()))
+  {
+    return response.status(400).json({ 
+      error: 'User alredy added to the phonebook' 
     })
   }
 
