ae10af07ec8e4071743f17d4e3d8cf8f40ce55cc
Diego Ibarra <diego12el@hotmail.com>
2025-12-29 18:44:15 -0300

    FSO 3.5 added the post and delete endpoints to the persons service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     { 
@@ -24,13 +25,15 @@ let persons = [
     }
 ]
 
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+
 
 
 app.get('/info', (request, response) => {
@@ -50,6 +53,40 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).send({ error: 'person not found' });
      }
   
+})
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+if (!body || !body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person);
+  response.json(person)
+})
+
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
